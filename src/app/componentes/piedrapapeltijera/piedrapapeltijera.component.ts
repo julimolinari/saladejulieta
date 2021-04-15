fc5e7e3e170ed37cd0293b3ed452f9e683ced66f
Julieta <julietamolinari85@gmail.com>
2021-04-14 22:53:31 -0300

    juego1

--- src/app/componentes/piedrapapeltijera/piedrapapeltijera.component.ts
+++ src/app/componentes/piedrapapeltijera/piedrapapeltijera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-piedrapapeltijera',
@@ -10,6 +11,38 @@ export class PiedrapapeltijeraComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  public jugar(valor : string){
+
+    var pcEleccion = ["piedra", "papel", "tijera"];
+    console.log(valor);
+    
+
+    var pcRespuesta = pcEleccion[Math.floor(Math.random() * pcEleccion.length)];
+    console.log(pcRespuesta);
+
+    if(valor == pcRespuesta){
+      Swal.fire({
+        icon: 'info',
+        text: valor+ ' VS ' + pcRespuesta,
+        title: 'Es un empate!'
+      })
+    }else if(valor=='piedra' && pcRespuesta=='tijera' || valor=='papel' && pcRespuesta=='piedra' || valor=='tijera' && pcRespuesta=='papel' ){
+      Swal.fire({
+        icon: 'success',
+        text: valor+ ' VS ' + pcRespuesta,
+        title: 'GANASTE!'
+      })
+    }else if(valor=='piedra' && pcRespuesta=='papel' || valor=='papel' && pcRespuesta=='tijera' || valor=='tijera' && pcRespuesta=='piedra' ){
+      Swal.fire({
+        icon: 'error',
+        text: valor+ ' VS ' + pcRespuesta,
+        title: 'Perdiste :('
+      })
+    }
+
   }
 
 }
